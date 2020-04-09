322475001de923e642352ce90906ee486e829198
Joshua Cole <joshuacole356@gmail.com>
2020-04-09 00:04:57 -0700

    Move render logic to PlayerHand

--- lib/game.rb
+++ lib/game.rb
@@ -4,12 +4,6 @@
 class Game
   attr_reader :players, :deck
 
-  # TODO: maybe this belongs in Hand
-  HANDS = [
-    { sets: [2, 3] },
-    { runs: [2, 4, true] }
-  ].freeze
-
   # TODO: figure out how many players require a third deck
   # TODO: need to reshuffle the discard pile when the deck is empty
   def initialize
@@ -19,30 +13,7 @@ class Game
   end
 
   def build_hand(player)
-    current_hand = Game::HANDS[player.current_hand]
-    sets = build_sets(current_hand[:sets])
-    runs = build_runs(current_hand[:runs])
-    PlayerHand.new(deck.cards.slice!(0, 11), sets, runs)
-  end
-
-  def build_runs(runs)
-    return nil if runs.nil?
-
-    result = []
-    runs.first.times do
-      Run.new(runs[1], runs[2])
-    end
-    result
-  end
-
-  def build_sets(sets)
-    return nil if sets.nil?
-
-    result = []
-    sets.first.times do
-      HandSet.new(sets[1])
-    end
-    result
+    PlayerHand.build(player.current_hand, deck)
   end
 
   def deal
@@ -78,19 +49,12 @@ class Game
       render_hand(player)
     end
   end
+  # rubocop:enable Metrics/MethodLength
+  # rubocop:enable Metrics/AbcSize
 
-  # TODO: clean up the repetition here
   def render_hand(player)
     player.hand.render
     puts ''
-    current_hand = Game::HANDS[player.current_hand]
-    sets = current_hand[:sets]
-    runs = current_hand[:runs]
-    sets_string = sets.nil? ? sets : "#{sets[0]} sets of #{sets[1]}"
-    runs_string = runs.nil? ? runs : ", #{runs[0]} runs of #{runs[1]}"
-    same_suit = runs && runs[3] ? ' same suit' : nil
-    puts "#{sets_string}#{runs_string}#{same_suit}"
+    PlayerHand.render(player.current_hand)
   end
-  # rubocop:enable Metrics/MethodLength
-  # rubocop:enable Metrics/AbcSize
 end
--- lib/hand.rb
+++ lib/hand.rb
@@ -4,12 +4,55 @@
 class PlayerHand
   attr_reader :sets, :runs, :cards
 
+  HANDS = [
+    { sets: [2, 3] },
+    { runs: [2, 4, true] }
+  ].freeze
+
   def initialize(cards, sets = nil, runs = nil)
     @sets = sets
     @runs = runs
     @cards = cards
     @down = false
     @piles = []
+  end
+
+  # Factory method
+  def self.build(hand_idx, deck)
+    current_hand = PlayerHand::HANDS[hand_idx]
+    sets = build_sets(current_hand[:sets])
+    runs = build_runs(current_hand[:runs])
+    new(deck.cards.slice!(0, 11), sets, runs)
+  end
+
+  def self.build_runs(runs)
+    return nil if runs.nil?
+
+    result = []
+    runs.first.times do
+      Run.new(runs[1], runs[2])
+    end
+    result
+  end
+
+  def self.build_sets(sets)
+    return nil if sets.nil?
+
+    result = []
+    sets.first.times do
+      HandSet.new(sets[1])
+    end
+    result
+  end
+
+  def self.render(hand_idx)
+    current_hand = PlayerHand::HANDS[hand_idx]
+    sets = current_hand[:sets]
+    runs = current_hand[:runs]
+    sets_string = sets.nil? ? sets : "#{sets[0]} sets of #{sets[1]}"
+    runs_string = runs.nil? ? runs : ", #{runs[0]} runs of #{runs[1]}"
+    same_suit = runs && runs[3] ? ' same suit' : nil
+    puts "#{sets_string}#{runs_string}#{same_suit}"
   end
 
   # Will be replaced by client code
