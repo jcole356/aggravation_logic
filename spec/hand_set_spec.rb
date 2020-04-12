1b3b2a5d10891f19293cab4a69f0e47746785073
Joshua Cole <joshuacole356@gmail.com>
2020-04-11 17:22:58 -0700

    Fix specs for hand_set

--- spec/hand_set_spec.rb
+++ spec/hand_set_spec.rb
@@ -14,9 +14,8 @@ RSpec.describe 'Hand::valid_move?' do
 
   it 'returns true if the card matches the set' do
     set = HandSet.new(3)
-    set.value = Card::VALUES[:five]
     card1 = Card.new(Card::SUITS[:diamonds], Card::VALUES[:five])
-    set.cards << card1
+    set.play(card1)
     card2 = Card.new(Card::SUITS[:hears], Card::VALUES[:five])
 
     expect(set.valid_move?(card2)).to eq(true)
@@ -24,9 +23,8 @@ RSpec.describe 'Hand::valid_move?' do
 
   it 'returns false if the card does not match set' do
     set = HandSet.new(3)
-    set.value = Card::VALUES[:five]
     card1 = Card.new(Card::SUITS[:diamonds], Card::VALUES[:five])
-    set.cards << card1
+    set.play(card1)
     card2 = Card.new(Card::SUITS[:diamonds], Card::VALUES[:three])
 
     expect(set.valid_move?(card2)).to eq(false)
@@ -35,11 +33,10 @@ RSpec.describe 'Hand::valid_move?' do
   context 'when the card is wild' do
     it 'returns true if there are at least two natural cards' do
       set = HandSet.new(3)
-      set.value = Card::VALUES[:five]
       card1 = Card.new(Card::SUITS[:diamonds], Card::VALUES[:five])
       card2 = Card.new(Card::SUITS[:hearts], Card::VALUES[:five])
-      set.cards << card1
-      set.cards << card2
+      set.play(card1)
+      set.play(card2)
       card3 = Wild.new(Card::SUITS[:hearts], Card::VALUES[:two])
 
       expect(set.valid_move?(card3)).to eq(true)
@@ -47,9 +44,8 @@ RSpec.describe 'Hand::valid_move?' do
 
     it 'returns false if are not at least two natural cards' do
       set = HandSet.new(3)
-      set.value = Card::VALUES[:five]
       card1 = Card.new(Card::SUITS[:diamonds], Card::VALUES[:five])
-      set.cards << card1
+      set.play(card1)
       card2 = Wild.new(Card::SUITS[:hearts], Card::VALUES[:two])
 
       expect(set.valid_move?(card2)).to eq(false)
@@ -59,11 +55,10 @@ RSpec.describe 'Hand::valid_move?' do
   context 'when the previous card is wild' do
     it 'returns true if the card matches a natural card' do
       set = HandSet.new(3)
-      set.value = Card::VALUES[:five]
       card1 = Card.new(Card::SUITS[:diamonds], Card::VALUES[:five])
       card2 = Card.new(Card::SUITS[:hearts], Card::VALUES[:five])
-      set.cards << card1
-      set.cards << card2
+      set.play(card1)
+      set.play(card2)
       card3 = Wild.new(Card::SUITS[:hearts], Card::VALUES[:two])
 
       expect(set.valid_move?(card3)).to eq(true)
@@ -71,9 +66,8 @@ RSpec.describe 'Hand::valid_move?' do
 
     it 'returns false if it does not match a natural card' do
       set = HandSet.new(3)
-      set.value = Card::VALUES[:five]
       card1 = Card.new(Card::SUITS[:diamonds], Card::VALUES[:five])
-      set.cards << card1
+      set.play(card1)
       card2 = Wild.new(Card::SUITS[:hearts], Card::VALUES[:two])
 
       expect(set.valid_move?(card2)).to eq(false)
